3ad87bcb6276afd9ec33332a4eea3c54292dbf36
Sandra <sandra@Sandras-Mac.local>
2018-03-21 16:54:35 +0100

    Lesson 056 done, added rear and front views

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Sandra on 3/21/18.
+//  Copyright © 2018 Sandra. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+    }
+
+    
+
+    
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Sandra on 3/21/18.
+//  Copyright © 2018 Sandra. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
