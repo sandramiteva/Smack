dea8b840e5d8720b5b74afb4c40150dcbe4ad3ac
Sandra <sandra@Sandras-Mac.local>
2018-04-03 16:09:06 +0200

    Lesson 66 done, added create new user request

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -11,12 +11,15 @@ import UIKit
 class CreateAccountVC: UIViewController {
 
     //Outlets
-    
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,19 +32,22 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccntPressed(_ sender: Any) {
-        guard let email = emailTxt.text , emailTxt.text != "" else {
-            return
-        }
-        guard let pass  = passwordTxt.text, passwordTxt.text != "" else {
-            return
-        }
+        guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass  = passwordTxt.text, passwordTxt.text != "" else { return }
+        
         AuthService.instance.registerUser(email: email, password: pass)
             { (success) in
                 if success {
                     AuthService.instance.loginUser(email: email, password: pass)
                     { (success) in
                         if success {
-                            print("logged in user!", AuthService.instance.authToken)
+                            AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                                if success{
+                                    print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                    self.performSegue(withIdentifier: UNWIND, sender: nil )
+                                }
+                            })
                         }
                         
                     }
